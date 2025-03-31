05ea9c764410872b5c43f5d45fc02583445d5b35
incognito0007 <ankit.22010787@viit.ac.in>
2025-03-31 16:35:39 +0530

    Basics | input | if-else | loops | datatypes | typeCasting

---
+++ Project/_01FirstJavaProgram/Primitives.java
@@ -0,0 +1,12 @@
+package _01FirstJavaProgram;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNumber = 22010787;
+        char letter = 'a';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 3.12345678912345;
+        long largeInteger = 12345678901234567L;
+        boolean check = false;
+    }
+}
